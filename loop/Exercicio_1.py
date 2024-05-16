c4476d5ee590c0ba425cccffb4d9c8aabdda5f87
Marquinnnhos001 <marcosdecesarepalazan@gmail.com>
2024-05-16 12:06:29 -0300

    Atualizaçao.Exercicio2.py

--- loop/Exercicio_1.py
+++ loop/Exercicio_1.py
@@ -1,20 +1,8 @@
-# Solicite ao Usuário que Informe o Número Inteiro
-n= int(input("Informe um número inteiro positivo N:"))
-       
-#Verifique se o número informado é positivo
-while n<=0:
-    n = int(input("Por favor, informe um número inteiro positivo N:"))
-
-#Variavel Soma
-soma=0
-
-#Solicite ao úsuario que digite N números e os soma
-for i in range(n):
-    numero =float(input("Digite um número:"))
-    soma += numero
-
-#Calcule a média aritmetica dos N números digitados
-media= soma/n
-
-#Imprime a Média
-print("A média dos números digitados é:",media)
+n=int(input("Informe quantos números serão calculados a média: "))
+soma = 0
+iteracao = 1
+while iteracao<=n:
+    numb=float(input("Digite um número: "))
+    soma += numb
+    iteracao += 1
+print (f"O valor da média é igual a : {soma/n}")
